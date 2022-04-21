c8d914dfec8564e55cc9442cdd8e6f3f18f2e198
Luis Vinicius <camperqueodeiacamper@gmail.com>
2022-04-21 13:02:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hello</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
